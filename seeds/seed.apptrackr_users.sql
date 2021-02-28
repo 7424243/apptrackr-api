13d016748aea01de0ca283292ae96e315d31ddee
Sam Sheets <sam.sheets94@gmail.com>
2021-02-28 14:35:10 -0700

    implemented bcrypt for passwords

--- seeds/seed.apptrackr_users.sql
+++ seeds/seed.apptrackr_users.sql
@@ -3,12 +3,12 @@ VALUES
     (
         'Sam Sheets',
         'samsheets',
-        'Sam1234!',
+        '$2a$12$XslM9.HYac6Hskbn0/kkp.6.v5kQr5fkWXlaGU/UTL275Ujk2cCem',
         '2021-02-28T00:00:00.000Z'
     ),
     (
         'Demo User',
         'demo',
-        'Demo1234!',
+        '$2a$12$lzQfsMeQRsHjAWlQTkIBduHY6h0SfRARE8TUKd.UcTbvLdV43FSVK',
         '2021-02-28T00:00:00.000Z'
     );
