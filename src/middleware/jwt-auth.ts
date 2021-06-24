7809df873914b162aa21b3cdc4bc128b6874eb86
Samantha Sheets <Samantha.Sheets@datashieldprotect.com>
2021-06-24 14:32:48 -0600

    convert jwt middleware to typescript

--- src/middleware/jwt-auth.ts
+++ src/middleware/jwt-auth.ts
@@ -1,6 +1,6 @@
-const AuthService = require('../auth/auth-service')
+import AuthService  from '../auth/auth-service'
 
-function requireAuth(req, res, next) {
+export const requireAuth = (req, res, next) => {
     const authToken = req.get('Authorization') || ''
     let bearerToken
     if(!authToken.toLowerCase().startsWith('bearer ')) {
@@ -26,8 +26,4 @@ function requireAuth(req, res, next) {
     } catch (error) {
         res.status(401).json({error: 'Unauthorized request'})
     }
-}
-
-module.exports = {
-    requireAuth
 }
